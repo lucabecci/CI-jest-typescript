51523ebdc891b7e3914c3b17981ec8550b810072
luca <beccibrian@gmail.com>
2021-02-24 13:10:38 -0300

    chore(@all): Dockerfile and tests added

---
+++ test/someFunc.test.ts
@@ -0,0 +1,13 @@
+import { add } from "../src/someFunc";
+describe("test in add function", () => {
+    test("should return 200 for add(100+100)", () => {
+        expect(add(100, 100)).toBe(200);
+    });
+    test("should return 325 for add(300+25)", () => {
+        expect(add(300, 25)).toBe(325);
+    });
+
+    test("should return 5", () => {
+        expect(add(3, 2)).toBe(5);
+    });
+});
